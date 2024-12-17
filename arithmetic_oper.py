5b7ce4412106d1af5c04aaf4ef8545739d6d7e5c
Mohamed Saud <86352274+mdsaud03@users.noreply.github.com>
2024-12-17 16:50:00 +0530

    Initial_commit

---
+++ arithmetic_oper.py
@@ -0,0 +1,36 @@
+a=7
+b=5
+
+#Addition (+)
+print("Addition",a+b)
+
+#Subtraction (-)
+print("Subtraction",a-b)
+
+#Multiplication (*)
+print("Multiplication",a*b)
+
+#Division (/)
+print("Division",a/b)
+
+#Floor Division (//)
+print("Floor Division",a//b)
+
+#Modulo (%)
+print("Modulo",a%b)
+
+#Exponentiation (**)
+print("Exponentiation",a**b)
+
+'''
+Output:
+
+Addition 12
+Subtraction 2
+Multiplication 35
+Division 1.4
+Floor Division 1
+Modulo 2
+Exponentiation 16807
+
+'''
